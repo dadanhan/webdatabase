e8b14346991582959640dd334c3be7ee8a17fbb9
DanHan <daniel.han@unsw.edu.au>
2024-10-07 18:40:33 +1100

    changes for amplify

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
+      <h1>Dan's test webpage that has been deployed...</h1>
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
